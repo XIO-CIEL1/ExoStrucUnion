08edbec1fe4d1f8946180c897b4ec5d90e7e1215
Tom Xiong <xiongtom151@gmail.com>
2024-11-26 09:29:56 +0100

    commit#2 Déclaration des variables, tableaux,définition des structures, énumerations et unions des éléments précédents.

--- Transgerbeur/Transgerbeurcpp.cpp
+++ Transgerbeur/Transgerbeurcpp.cpp
@@ -5,11 +5,45 @@
 
 #include<iostream>
 
+
 using namespace std;
+
+//énumeration
+enum EEtat {palette, rouleau };
+
+
+
+//unions des éléments précédents
+union UContenu;
+ 
 
 int main()
 {
 	//affichage du nom du programme sur la console
 	cout << "---------------Programme Transgerbeur------------------" << endl;
+
+	//déclaration du tableau
+	int Scasier[9] = { 1,2,3,4,5,6,7,8,9 };
+	//declaration des variables
+		int pX;
+		int pY;
+		int poids;
+		char reference = 19;
+		float longueur;
+		int numero;
+		
+
+	//déinition des structures
+		struct SPalette { int poids; char reference; };
+		struct SRouleau { float longueur; int numero; };
+		struct SCasier { int pX; int pY; };
+	 
+
+
+
+
+	
+
+
 }
 
