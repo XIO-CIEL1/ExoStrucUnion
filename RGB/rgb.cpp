e1bf9f654f1feda498d1131fb5bcbcde4d978feb
Tom Xiong <xiongtom151@gmail.com>
2024-11-26 08:28:58 +0100

    commit#1 ajout des fichiers .cpp et des projets

---
+++ RGB/rgb.cpp
@@ -0,0 +1,15 @@
+// Date : 26/11/2024
+// nom de l'utilisateur : XIONG Tom
+//TP5 Struct enum union : Partie RGB
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//affichage du nom du programme sur la console
+	cout << "---------------Programme RGB---------------------" << endl;
+}
+
